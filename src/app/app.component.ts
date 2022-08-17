2adc8e9e618bd5620d2a7ac779722b4dc7cd62bf
juliocsantaman-tec <juliocsantaman@tec.mx>
2022-08-17 16:05:16 -0500

    Doing tests for class Calculator and knowing some matchers of Jasmine.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-unit-testing-services';
+
+  ngOnInit() {
+    const calculator = new Calculator();
+
+    const response = calculator.multiply(4, 5);
+  }
 }
---
+++ src/app/calculator.spec.ts
@@ -0,0 +1,54 @@
+import { Calculator } from "./calculator";
+
+describe('Test for Calculator', () => {
+  it('#multiply should return a nine.', () => {
+    // AAA.
+    // Arrange.
+    const calculator = new Calculator();
+    // Act.
+    const response = calculator.multiply(3, 3);
+    // Assert.
+    expect(response).toEqual(9);
+
+  });
+
+  it('#multiply should return a four.', () => {
+    // AAA.
+    // Arrange.
+    const calculator = new Calculator();
+    // Act.
+    const response = calculator.multiply(1, 4);
+    // Assert.
+    expect(response).toEqual(4);
+
+  });
+
+  it('#divide should return a some numbers.', () => {
+    // AAA.
+    // Arrange.
+    const calculator = new Calculator();
+
+    expect(calculator.divide(6, 3)).toEqual(2);
+    expect(calculator.divide(5, 2)).toEqual(2.5);
+
+  });
+
+  it('#test matchers', () => {
+    const name = 'Julio';
+    let name2;
+    expect(name).toBeDefined();
+    expect(name2).toBeUndefined();
+
+    expect(1 + 3 === 4).toBeTruthy(); // 4
+    expect(1 + 1 === 3).toBeFalsy();
+
+    expect(5).toBeLessThan(10); // true
+    expect(20).toBeGreaterThan(10); // true
+
+    expect('123456').toMatch(/123/);
+    expect(['apples', 'oranges', 'pears']).toContain('oranges'); // true
+  });
+
+});
+
+
