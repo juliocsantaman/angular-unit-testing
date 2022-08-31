d229f58e12ca06642be7f43ae73dc9a29d6b527d
juliocsantaman <juliocsantaman@gmail.com>
2022-08-30 21:59:47 -0500

    CI

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    customLauchers: {
+    customLaunchers: {
       ChromeCI: {
         base: Chrome,
         flags: ['--no-sandbox']
